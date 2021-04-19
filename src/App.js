51fb4af90f3c1b8af05abcc07f0a97c9573ddb14
Krishna Silalahi <krishna.silalahi@gmail.com>
2021-04-19 13:06:14 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { GithubLoginButton, GoogleLoginButton } from "react-social-login-buttons";
+import { Button, Form, Grid, Input, Segment, Divider } from 'semantic-ui-react'
+import {Component} from "react";
+import User from './User/User';
+import {createRef} from "react";
+
+class App extends Component {
+    state = {
+        user: {
+            nickname: 'None',
+            counter: 0
+        }
+    };
+    textInput = createRef();
+
+    updateNickname = (event) => {
+        this.setState({
+            user: {
+                nickname: event.target.value,
+                counter: this.state.user.counter
+            }
+        })
+    }
+
+    updateCounter = () => {
+        this.setState({
+            user: {
+                nickname: this.state.user.nickname,
+                counter: this.state.user.counter + 1
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>
+                    <Grid.Column style={{maxWidth: 400}}>
+                        <Form size='large'>
+                            <Segment stacked>
+                                <GoogleLoginButton/>
+                                <GithubLoginButton/>
+                                <Divider />
+                                Nickname: <Input fluid ref={this.textInput} placeholder={'Enter Nickname'} onChange={this.updateNickname.bind(this)} />
+                                <Button fluid primary onClick={this.updateCounter}>Increment Counter</Button>
+                                <Divider />
+                                <User nickname={this.state.user.nickname} counter={this.state.user.counter} />
+                            </Segment>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/User/User.js
@@ -0,0 +1,10 @@
+
+const user = (props) => {
+    return (
+        <div>
+            <p>Nickname: {props.nickname} and Counter: {props.counter}</p>
+        </div>
+    );
+}
+
+export default user;
